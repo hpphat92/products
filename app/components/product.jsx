e50cc8140561ffeda842faca1b9f57cd09e8742a
andy <andy@d8ii.com>
2016-03-29 15:18:18 +0700

    first commit

---
+++ app/components/product.jsx
@@ -0,0 +1,12 @@
+var React = require('React');
+var Product = React.createClass({
+  render: function () {
+    return (
+      < div >
+        This is product data
+      </div>
+    )
+  }
+});
+
+module.exports = Product;
---
+++ app/components/products.jsx
@@ -0,0 +1,14 @@
+var Product = require('./product');
+var Products = React.createClass({
+
+  render: function () {
+    return (
+      < Product>
+      </Product>
+    )
+  }
+});
+
+React.render(<Products/>, document.body);
+
+
