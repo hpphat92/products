cb9da9a4659fef815bcb026533aaafb6d052ba7f
NOIS Phat Tran <phat@footballindex.co.uk>
2016-03-29 15:37:55 +0700

    fix library deps

--- app/js/products.js
+++ app/js/products.js
@@ -1,3 +1,4 @@
+var React = require('react');
 var Product = require('./product');
 var Products = React.createClass({displayName: "Products",
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 var gulp  = require('gulp');
 var react = require('gulp-react');
-
+var browserify = require('gulp-browserify');
 gulp.task('jsx', function(){
     return gulp.src( 'app/components/*.jsx' )
         .pipe( react() )
         .pipe( gulp.dest( 'app/js' ) )
     ;
 });
+
+gulp.task( 'browserify', function() {
+    return gulp.src( 'app/js/products.js' )
+        .pipe( browserify() )
+        .pipe( gulp.dest( 'app' ) )
+    ;
+});
+
+gulp.task( 'build', ['jsx', 'browserify'] );
